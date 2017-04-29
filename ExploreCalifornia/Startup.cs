fc347ffef783325cc1c899391365b8a00d900dfe
Istvan Marki <istv0050@edu.easj.dk>
2017-04-29 14:32:58 +0200

    view logic with injectable srvices

--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExploreCalifornia.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -32,6 +33,8 @@ namespace ExploreCalifornia
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //registering services
+            services.AddTransient<FormattingService>();
             /*
              * Dependency Injection options:
              *  TRANSIENT: 
